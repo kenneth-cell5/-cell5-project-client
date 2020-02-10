a09c705a2e507353353bb8284dbd8570e6a77fdc
Kenneth Burla <kenneth@quanta.com.ph>
2020-02-11 03:12:21 +0800

    Initial Commit

---
+++ src/components/ContentWrapper.js
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "../assets/css/ContentWrapper.module.css";
+
+export default function ContentWrapper() {
+  return (
+    <>
+      <div className={style.contentWrapper}></div>
+    </>
+  );
+}
